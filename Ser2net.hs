2acc30c27ae6acc25c5aea52bfae141346c9a1ec
Jeff Lasslett <jeff.lasslett@gmail.com>
2013-09-05 18:10:09 +1000

    Add daemon management functions (start, stop, restart)

--- Ser2net.hs
+++ Ser2net.hs
@@ -4,14 +4,27 @@ import System.Posix.Types
 import System.Posix.Process
 import System.Posix.Signals
 
+data Action = StartDaemon | StopDaemon | RestartDaemon deriving ( Eq, Show )
+
 reloadConfig :: ProcessID -> IO ()
 reloadConfig =
   signalProcess sigHUP
 
 restartDaemon :: IO ()
-restartDaemon =
+restartDaemon = manageDaemon RestartDaemon
+
+stopDaemon :: IO ()
+stopDaemon = manageDaemon StopDaemon
+
+manageDaemon :: Action ->  IO ()
+manageDaemon a =
   let
-    p = executeFile "service" True [ "ser2net", "restart" ] Nothing
+    action = case a of
+               StartDaemon -> "start"
+               StopDaemon -> "stop"
+               RestartDaemon -> "restart"
+
+    p = executeFile "service" True [ "ser2net", action ] Nothing
   in
     do 
       pid <- forkProcess p
